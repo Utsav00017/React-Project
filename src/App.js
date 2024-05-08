c896aae919e00311566161a4ec09db83b35a75a0
Utsav00017 <utsavbutani1234@gmail.com>
2024-05-08 16:10:43 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Wel come To Host </h1>
+        <h1>Welcome to react application </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
